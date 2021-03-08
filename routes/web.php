ba622da709b1a7e04bf3f82ea591d35ac8cea5a0
Abiduzzaman Abid <itisabidss@gmail.com>
2021-03-08 17:17:31 +0600

    Uploading Files

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/logout', [App\Http\Controllers\HomeController::class, 'logout']);
+
+
+
+Route::get('/all-categories', function(){
+    return view('admin.all-categories');
+});
